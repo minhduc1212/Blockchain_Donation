52b35a3f8adc72aec5fe8170526b1b4aad459803
Đức Nguyễn Minh <minhduc6e12@gmail.com>
2025-04-24 19:37:21 +0700

    The First

---
+++ get_utxo.py
@@ -0,0 +1,20 @@
+from pycardano import (
+    Address,
+    BlockFrostChainContext
+)
+ 
+context = BlockFrostChainContext(
+    "previewJhNPT5QxoI6h2TujleChtJ7qTxNZqSAZ",
+    base_url="https://cardano-preview.blockfrost.io/api/",
+)
+
+address = Address.from_primitive("addr_test1qrufwlthgmawesrtj7ykvfh30kl6kjn3cz8sla769tjyngsxu7u7lu4xqq2jzkkc9ge0s7wra3yn2lztzfeh7xnu4ujs8l2avj")
+                                    
+try:
+    utxos = context.utxos(address)
+    print("UTXOs for the address:")
+    for utxo in utxos:
+        print(utxo) 
+
+except Exception as e:
+    print("Error fetching UTXOs or processing transaction:", e)
